11f2c7dd1934fe89ad3bd786e68037aa54c3bdba
SarahRakotomanana <s.rako.manana@gmail.com>
2025-03-03 17:59:16 +0100

    added devise

--- db/schema.rb
+++ db/schema.rb
@@ -28,10 +28,16 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_03_143800) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "email"
     t.string "phone_number"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "vans", force: :cascade do |t|
