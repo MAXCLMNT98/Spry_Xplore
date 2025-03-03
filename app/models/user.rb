6c59bf265b6bbfe5cf4f5404e68b53bd18353634
Maxime CLEMENT <com.maximeclement@gmail.com>
2025-03-03 16:41:43 +0100

    currently working models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  has_many :bookings, dependant: :destroy
-  has_many :vans, dependant: :destroy
+  has_many :bookings, dependent: :destroy
+  has_many :vans, dependent: :destroy
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, format: { /^([^@]+)@([^@]+)\.(\w+)$/ }
-  validates :phone_number: presence: true, format: { /^(0|\+33[\s-]?)[1-9][\s-]?(\d{2}[\s-]?){4}$/ }
+  validates :email, presence: true, format: { with:  URI::MailTo::EMAIL_REGEXP }
+  validates :phone_number, presence: true, format: { with: /\A(0|\+33[\s-]?)[1-9][\s-]?(\d{2}[\s-]?){4}\z/ }
 end
