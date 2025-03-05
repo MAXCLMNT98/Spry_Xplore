f20f9b507a825d7be75f9147478b5c0ec22bb851
Chloe Cosson <cosson.chloe@gmail.com>
2025-03-05 12:07:31 +0100

    user dashboard for booked and rented-out vans

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,29 +1,30 @@
 class BookingsController < ApplicationController
 
-     def index
-        @bookings = Booking.all
-        # @bookings_as_visitor = current_user.bookings_as_visitor
-        # @bookings_as_owner = current_user.bookings_as_owner
-      end
+    def index
+      @bookings_as_visitor = current_user.bookings_as_visitor
+      @bookings_as_owner = current_user.bookings_as_owner
+    end
 
     def new
         @van = Van.find(params[:van_id])
         @booking = Booking.new
     end
-    def create
-        @booking = Booking.new(booking_params)
-        @booking.user = current_user
-        @van = Van.find(params[:van_id])
-        @booking.van = @van
-        if @booking.save
-          redirect_to "/", notice: "tu as réserver un van"
-        else
-          render :new, status: :unprocessable_entity
-        end
-      end
-end
-private
 
-def booking_params
-  params.require(:booking).permit(:start_date, :end_date)
+    def create
+      @booking = Booking.new(booking_params)
+      @booking.user = current_user
+      @van = Van.find(params[:van_id])
+      @booking.van = @van
+      if @booking.save
+        redirect_to "/", notice: "tu as réserver un van"
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
--- app/models/van.rb
+++ app/models/van.rb
@@ -2,6 +2,7 @@ class Van < ApplicationRecord
   CATEGORIES = ["gasoline", "diesel", "hybrid", "electric", "hydrogen"]
   belongs_to :user
   has_one_attached :photo
+  has_many :bookings, dependent: :destroy
 
   validates :fuel, presence: true, inclusion: { in: CATEGORIES }
   validates :model, presence: true
