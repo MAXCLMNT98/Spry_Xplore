5cd65b8628be6574d5668ed2a8235418cccadc03
Chloe Cosson <cosson.chloe@gmail.com>
2025-03-05 11:24:18 +0100

    Add dotenv-rails

--- app/models/van.rb
+++ app/models/van.rb
@@ -1,6 +1,7 @@
 class Van < ApplicationRecord
   CATEGORIES = ["gasoline", "diesel", "hybrid", "electric", "hydrogen"]
   belongs_to :user
+  has_one_attached :photo
 
   validates :fuel, presence: true, inclusion: { in: CATEGORIES }
   validates :model, presence: true
