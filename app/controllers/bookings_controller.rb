2130e9ac1d1314fcc5b4f0f69ba7bd10847b9dae
SarahRakotomanana <s.rako.manana@gmail.com>
2025-03-05 11:33:14 +0100

    added links in all vans

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,27 +1,28 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+    # @bookings_as_visitor = current_user.bookings_as_visitor
+    # @bookings_as_owner = current_user.bookings_as_owner
+  end
 
-     def index
-        @bookings = Booking.all
-        # @bookings_as_visitor = current_user.bookings_as_visitor
-        # @bookings_as_owner = current_user.bookings_as_owner
-      end
+  def new
+    @van = Van.find(params[:van_id])
+    @booking = Booking.new
+  end
 
-    def new
-        @van = Van.find(params[:van_id])
-        @booking = Booking.new
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @van = Van.find(params[:van_id])
+    @booking.van = @van
+    if @booking.save
+      redirect_to "/", notice: "Vous avez réservé un van"
+    else
+      render :new, status: :unprocessable_entity
     end
-    def create
-        @booking = Booking.new(booking_params)
-        @booking.user = current_user
-        @van = Van.find(params[:van_id])
-        @booking.van = @van
-        if @booking.save
-          redirect_to "/", notice: "tu as réserver un van"
-        else
-          render :new, status: :unprocessable_entity
-        end
-      end
+  end
 end
+
 private
 
 def booking_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_04_163549) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_05_102705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_04_163549) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["user_id"], name: "index_vans_on_user_id"
   end
 
